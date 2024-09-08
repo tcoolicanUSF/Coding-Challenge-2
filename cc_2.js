3bb3327926bd994cc984848e3142499a326c9414
tcoolican <tcoolican@usf.edu>
2024-09-08 11:42:32 -0400

    Calculatethe tip

--- cc_2.js
+++ cc_2.js
@@ -1,1 +1,11 @@
+// Coding Challenge 2
+// Calculate the tip
+let bill = 275;
+let tip = bill * (bill > 50 ? 0.15 : 0.10);
+let total = bill + tip;
+console.log("Bill: $" + bill);
+console.log("Tip: $" + tip);
+console.log("Total: $" + total);
+
+
 
