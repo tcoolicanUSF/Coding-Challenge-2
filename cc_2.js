c536d70895cb47be0e0ea02bd9315daf2599fac9
tcoolican <tcoolican@usf.edu>
2024-09-08 12:00:11 -0400

    edit

--- cc_2.js
+++ cc_2.js
@@ -1,7 +1,7 @@
 // Coding Challenge 2
 // Calculate the tip
 let bill = 275;
-let tip = bill * (bill > 50 ? 0.15 : 0.10);
+let tip = bill * (bill > 50 ? 0.15 : 0.20);
 let total = bill + tip;
 console.log("Bill: $" + bill);
 console.log("Tip: $" + tip);
@@ -9,6 +9,12 @@ console.log("Total: $" + total);
 
 //Output details
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${total}`);
+
+//Create a function
+function calculateTip(bill) {
+    return bill * (bill > 50 ? 0.15 : 0.20);
+}
+calculateTip(100)
 
 
 
